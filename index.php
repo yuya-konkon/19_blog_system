7e8db1dbda440efeb5b685101931e287a59df01c
yuya-konkon <yuuuuya08@icloud.com>
2020-03-09 16:01:25 +0900

    add header

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+
+session_start();
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -16,6 +23,25 @@
   <div class="flex-col-area">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
       <a href="http://localhost/19_blog_system/index.php" class="navbar-brand">Camp Blog</a>
+      <div class="collapse navbar-collapse" id="navbarToggle">
+        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+          <?php if ($_SESSION['id']) : ?>
+            <li class="nav-item">
+              <a href="sign_out.php" class="nav-link">ログアウト</a>
+            </li>
+            <li class="nav-item">
+              <a href="new.php" class="nav-link">New Post</a>
+            </li>
+          <?php else : ?>
+            <li class="nav-item">
+              <a href="sign_in.php" class="nav-link">ログイン</a>
+            </li>
+            <li class="nav-item">
+              <a href="sign_up.php" class="nav-link">アカウント登録</a>
+            </li>
+          <?php endif; ?>
+        </ul>
+      </div>
     </nav>
 
     <footer class="footer font-small bg-dark">
